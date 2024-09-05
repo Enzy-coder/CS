db30021c1bbdd8a1d7f23209a51c421845b44ca5
Muhammadali5326 <zulqurnain5326@gmail.com>
2024-09-05 08:32:11 +0500

    updated the cat image issue

--- resources/views/admin/continents/country-categories.blade.php
+++ resources/views/admin/continents/country-categories.blade.php
@@ -89,13 +89,12 @@
                     <div class="row cats">
                         @foreach ($product_categories as $product_category)
                             @php 
-                            dd($product_category);
-                                $image_path = optional($product_category->category)->image ? optional($product_category->category->image)->path : 'no-image.png';
+                                $image_path = optional($product_category->category)->image ? 'assets/uploads/media-uploader/'.optional($product_category->category->image)->path : 'no-image.png';
                             @endphp
                             <div class="col-md-1 text-center cat-click cursor-pointer" data-id="{{$product_category->category_id}}"
                                 data-slug="{{optional($product_category->category)->slug}}">
                                 <div class="cat-block">
-                                    <img src="{{asset('assets/uploads/media-uploader/'.$image_path)}}" class="cat-image">
+                                    <img src="{{asset($image_path)}}" class="cat-image">
                                 </div>
                                 <b class="category-name">
                                     {{optional($product_category->category)->name}}
