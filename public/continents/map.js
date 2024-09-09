08dbde88b71ee9f66fa5422224e65a2111be2778
Muhammadali5326 <zulqurnain5326@gmail.com>
2024-09-09 11:25:02 +0500

    updated

--- public/continents/map.js
+++ public/continents/map.js
@@ -183,8 +183,6 @@ $(document).ready(function() {
                     autoplay: true,
                     autoplaySpeed: 6000,
                     responsive: [
-                        { breakpoint: 1024, settings: { slidesToShow: 4 } },
-                        { breakpoint: 768, settings: { slidesToShow: 3 } },
                         { breakpoint: 480, settings: { slidesToShow: 1 ,slidesToScroll: 1} }
                     ]
                 }).on('init', function(event, slick) {
